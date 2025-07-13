08f77441a079526f69e98d10ba531c255b42dafe
whelanh <brickhousedevelopers@gmail.com>
2025-07-13 04:36:14 -0400

    Update install_CachyOS_Steps.sh

--- install_CachyOS_Steps.sh
+++ install_CachyOS_Steps.sh
@@ -352,5 +352,7 @@ echo ""
 echo "3. Consider enabling RDP if needed"
 echo ""
 echo "4. Reboot your system if you installed AppArmor"
+echo ""
+echo "5. For WOL, make sure sudo ethtool -s <interface_name> wol g
 
 print_status "All done! Enjoy your CachyOS setup!"

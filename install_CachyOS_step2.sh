81f0a0bd0c67a70acd174e0b295d6e222807ad89
whelanh <brickhousedevelopers@gmail.com>
2025-07-19 20:21:57 -0400

    Update install_CachyOS_step2.sh

--- install_CachyOS_step2.sh
+++ install_CachyOS_step2.sh
@@ -100,7 +100,7 @@ fi
 print_section "Flatpak Applications"
 if ask_yes_no "Install Flatpak applications?"; then
     print_status "Installing Flatpak applications..."
-    flatpak install -y be.alexandervanhee.gradia com.github.xournalpp.xournalpp org.kde.kmymoney com.jeffser.Alpaca dev.zed.Zed-Preview io.github.benini.scid io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager
+    flatpak install -y be.alexandervanhee.gradia com.github.xournalpp.xournalpp org.kde.kmymoney dev.zed.Zed-Preview io.github.benini.scid io.github.dvlv.boxbuddyrs com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager
     print_status "Flatpak applications installed"
 fi
 
